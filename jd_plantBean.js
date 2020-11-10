e837086cd20f8f50725316e9fe7f077955d00ea9
znz1992 <znznan1992@gmail.com>
2020-11-10 23:52:02 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -1,4 +1,4 @@
 var request = require('request');
-request('https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_818.js', function (error, response, body) {
+request('https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_plantBean.js', function (error, response, body) {
   eval(response.body)
 })
