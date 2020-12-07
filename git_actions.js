135a0749dbbc7f41b4a6b2a184ee64f3b4c4a6c9
znz1992 <znznan1992@gmail.com>
2020-12-07 22:05:56 +0800

    Update git_actions.js

--- git_actions.js
+++ git_actions.js
@@ -1,18 +1,4 @@
-const message=$prefs.valueForKey('jd_gitactions')
-console.log(message)
 
-async function browserForTurntableFarm2(type) {
-console.log(type);
-const myRequest = {
-    url: type,
-    method: method,
-    headers: headers,
-    body: body
-};
+console.log("打印了")
 
-$task.fetch(myRequest).then(response => {
-    console.log(response.statusCode + "\n\n" + response.body);
-}, reason => {
-    console.log(reason.error);
-});
-}
+
